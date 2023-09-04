db4d13117f73393e9ba612f9139dda7fcc38ae6a
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-04 13:52:38 +0000

    copied program

--- .ccls-cache/@home@runner@Ch11Q1P3-pointers-in-expressions/main.c
+++ .ccls-cache/@home@runner@Ch11Q1P3-pointers-in-expressions/main.c
@@ -1,6 +1,18 @@
+// More on Pointers
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int i1, i2;
+  int *p1,*p2;
+
+  i1 = 5;
+  p1 = &i1;
+  i2 = *p1 / 2 + 10;
+  p2 = p1;
+
+  printf ("i1 = %d, i2 = %d, *p1 = %d, *p2 = %d\n", i1, i2, *p1, *p2);
+  
   return 0;
 }
